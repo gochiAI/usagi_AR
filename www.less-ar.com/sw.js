a5a25a0bc1dd990ee8b39329076091c6a05bcad3
gochi_AI <yumemi1204@protonmail.com>
2023-03-24 09:56:48 +0000

    i

--- www.less-ar.com/sw.js
+++ www.less-ar.com/sw.js
@@ -1,52 +1,53 @@
 // https://qiita.com/yhrym/items/f31669d48688d32155b4
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 console.log(`Workbox ${workbox ? 'loaded' : 'failed to load'}`);
+workbox.core.setCacheNameDetails({ prefix: 'usagi_AR' });
+workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
+// Skip over the waiting lifecycle stage.
 workbox.core.skipWaiting();
-
+// clientsClaim() allows a service worker to take control of the page immediately.
 workbox.core.clientsClaim();
 
-workbox.navigationPreload.enable();
-
-// ------------------  runtime caching starts ---------------------
-//workbox-strategies
-// AR imgages
-// support by chatgpt
+// Use cache-first strategy to cache images
 workbox.routing.registerRoute(
-  // 画像ファイルを取得するパスを指定
-  /https:\/\/gochiai\.github\.io\/usagi_AR\/aws\.gochiusa\.com\/.*\.(?:png|jpg|webp|svg)/,  // キャッシュストラテジーを指定
+  /https:\/\/gochiai\.github\.io\/usagi_AR\/aws\.gochiusa\.com\/.*\.(?:png|jpg|webp|svg)/,
   new workbox.strategies.CacheFirst({
-    cacheName: 'AR images', // キャッシュ名を指定
+    cacheName: 'usagi_AR-images',
     plugins: [
       new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [0, 200]
-      })]
-    }));
-
-
-// icon
-workbox.routing.registerRoute(
-  /https:\/\/gochiai\.github\.io\/usagi_AR\/www\.less-ar\.com\/icons\/.*\.(?:png|ico|xml|svg)/,  // キャッシュストラテジーを指定
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'icons',
+        statuses: [0, 200],
+      }),
+    ],
   })
 );
-//pages
+
+// Cache pages with cache-first strategy
 workbox.routing.registerRoute(
-  /https:\/\/gochiai\.github\.io\/usagi_AR\/www\.less-ar\.com\/.*\.(?:html)/,  // キャッシュストラテジーを指定
+  /https:\/\/gochiai\.github\.io\/usagi_AR\/www\.less-ar\.com\/.*\.(?:html)/,
   new workbox.strategies.CacheFirst({
-    cacheName: 'pages', // キャッシュ名を指定
+    cacheName: 'usagi_AR-pages',
     plugins: [
       new workbox.cacheableResponse.CacheableResponsePlugin({
-        statuses: [0, 200]
-      })]})
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
 
-
-// manifest
+// Cache icons with stale-while-revalidate strategy
 workbox.routing.registerRoute(
-  /https:\/\/gochiai\.github\.io\/usagi_AR\/www\.less-ar\.com\/site\.json/,  // キャッシュストラテジーを指定
-  new workbox.strategies.StaleWhileRevalidate()
+  /https:\/\/gochiai\.github\.io\/usagi_AR\/www\.less-ar\.com\/icons\/.*\.(?:png|ico|xml|svg)/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'usagi_AR-icons',
+  })
+);
+
+// Cache manifest with stale-while-revalidate strategy
+workbox.routing.registerRoute(
+  /https:\/\/gochiai\.github\.io\/usagi_AR\/www\.less-ar\.com\/site\.json/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'usagi_AR-manifest',
+  })
 );
 
 
@@ -79,3 +80,14 @@ workbox.precaching.precacheAndRoute([
   './img/camera/permission_left.png',
   './img/camera/permission_top.png'
 ]);
+
+workbox.routing.setDefaultHandler(
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'default',
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+);
